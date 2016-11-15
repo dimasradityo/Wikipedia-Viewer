6458533ce58f93a7d5007212275615b4b60c623b
Dimas Radityo <dimsrad307@gmail.com>
2016-11-15 16:17:02 +0700

    Added search functionality

--- controller.js
+++ controller.js
@@ -1,13 +1,30 @@
-var app = angular.module('WikipediaApp', []);
-app.controller('mainController', function ($scope, $http) {    
-   var url = "https://en.wikipedia.org//w/api.php?action=opensearch&format=json&search=popeye&limit=10&callback=JSON_CALLBACK"
-   $http.jsonp(url)
-    .success(function (json) {
-      $scope.entries = json;
-      console.log($scope.entries);       
-   });
-});
 
+var app = angular.module('WikipediaApp', []);
+
+app.controller('mainController', function ($scope, $http) {
+
+  $scope.display = function () {
+    var tes = '';
+    tes = $scope.search;
+    var url = "https://en.wikipedia.org//w/api.php?action=opensearch&format=json&search=" + tes + "&limit=10&callback=JSON_CALLBACK";
+
+    $http.jsonp(url)
+      .success(function (json) {
+        $scope.entries = json;
+        var names = json[1];
+        var descs = json[2];
+        var links = json[3];
+
+        $scope.repeatData = names.map(function (val, index) {
+          return {
+            name: val,
+            desc: descs[index],
+            link: links[index]
+          }
+        });
+      });
+  };
+});
 
 
 
